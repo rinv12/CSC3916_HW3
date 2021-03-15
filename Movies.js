85477231c90865971cb78bddc3d92db728ff7751
rinv12 <marione1012@gmail.com>
2021-03-14 19:42:15 -0600

    almost done

---
+++ Movies.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+try{
+    mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log('connected'));
+}catch (error){
+    console.log('cannot connect');
+}
+mongoose.set('useCreateIndex', true);
+
+var movieSchema = new Schema({
+    title:{
+        type: String,
+        trim: true,
+        required: 'Title required',
+    },
+    year_released:{
+        type: String,
+        trim: true,
+        required: 'Year released is required',
+    },
+    genre:{
+        type: String,
+        required: true,
+        enum:["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Thriller", "Western"],
+    },
+    actors:
+    [
+        {actor_name: {type: String, required: true}, character_name:{type:String, required: true}},
+        {actor_name: {type: String, required: true}, character_name:{type:String, required: true}},
+        {actor_name: {type: String, required: true}, character_name:{type:String, required: true}}]
+});
+
+//exporting model back to the server
+module.exports = mongoose.model('Movies', movieSchema)
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*
-CSC3916 HW2
+CSC3916 HW3
 File: Server.js
 Description: Web API scaffolding for Movie API
  */
@@ -12,6 +12,7 @@ var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
+var Movie = require('./Movies');
 
 var app = express();
 app.use(cors());
@@ -40,7 +41,8 @@ function getJSONObjectForMovieRequirement(req) {
     return json;
 }
 
-router.post('/signup', function(req, res) {
+router.route('/signup')
+    .post(function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
     } else {
@@ -60,9 +62,13 @@ router.post('/signup', function(req, res) {
             res.json({success: true, msg: 'Successfully created new user.'})
         });
     }
-});
+})
+    .all(function (req, res){
+        res.json({success: false, msg: 'This method is not supported'});
+    });
 
-router.post('/signin', function (req, res) {
+router.route('/signin')
+    .post(function (req, res) {
     var userNew = new User();
     userNew.username = req.body.username;
     userNew.password = req.body.password;
@@ -85,6 +91,63 @@ router.post('/signin', function (req, res) {
     })
 });
 
+router.route('/movies')
+    .delete(authJwtController.isAuthenticated, function (req, res){
+        if(req.body.title){
+            Movie.remove(req.body.title, function(err, movie){
+                if(err){
+                    res.status(403).json({success:false, message:"error cant delete movie"});
+                }else if(!movie){
+                    res.status(403).json({success:false, message: "cant find movie"});
+                }else{
+                    res.status(200).json({success:true, message: "movie deleted"})
+                }
+            })
+        }else{
+            res.json({success:false, message:"provide movie title to delete"});
+        }
+    })
+    .get(authJwtController.isAuthenticated, function (req, res){
+        if(req.body){
+            Movie.find(req.body.select("title year_released genre actors").exec(function(err, movie){
+                if(err){
+                res.status(403).json({success:false, message:"unable to find the movie"});
+            }if(movie){
+                res.status(200).json({success:true, message: "movie found", movie: movie});
+                }else{
+                    res.status(404).json({success:false, message: "movie not found"});
+                }
+            }))
+        }else{
+            res.json({success: false, message: "provide a movie title to show"});
+        }
+    })
+    .post(authJwtController.isAuthenticated, function (req,res){
+        console.log(req.body);
+        if(req.body.title || req.body.year_released || req.body.genre || req.body.actors[0] || req.body.actors[1] || req.body.actors[2]){
+            var mov = new Movie();
+
+            mov.title = req.body.title;
+            mov.year_released = req.body.year_released;
+            mov.genre = req.body.genre;
+            mov.actors = req.body.actors;
+            mov.save(function(err){
+                if(err){
+                    if(err.code == 11000){
+                        return res.json({success: false, message: "username already exist"});
+                    }else{
+                        return res.json(err);
+                    }
+                }
+            })
+            res.json({success:true, message: "movie created"});
+        }else{
+            res.json({success: false, message: "title, year released, genre, and three actors required"});
+        }
+    })
+    .all(function(req, res){
+        res.json({success:false, message: "route not supported"});
+    })
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
